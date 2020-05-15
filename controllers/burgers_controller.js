4516adcfbf4c03c544570bea94392e484469a223
Mariana Barnett <marianabarnett3@gmail.com>
2020-05-15 01:37:12 -0400

    fixed typo

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,7 +16,7 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burger", function(req, res) {
   burger.create([
     "name", "devoured"
   ], [
@@ -27,7 +27,7 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burger/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
