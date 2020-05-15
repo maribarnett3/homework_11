283ac25867618124116158b896ec01ed59a7ce53
Mariana Barnett <marianabarnett3@gmail.com>
2020-05-15 01:41:12 -0400

    fixed name in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burger", function(req, res) {
   burger.create([
-    "name", "devoured"
+    "burger_name", "devoured"
   ], [
     req.body.name, false
   ], function(result) {
